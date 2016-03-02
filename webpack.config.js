47f6b93a4559ed4eac1c58c7d6eb0a08ac217ad7
Patrick Puritscher <blackout289@gmail.com>
2016-03-02 23:28:21 +0100

    remove distribution package

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         include: [
           path.join(__dirname, 'src'),
           path.join(__dirname, 'example'),
-        ]
+        ],
       },
       {
         test: /\.css$/,
